d1ddaea3106bdd4d9127e1d919ee42fdc4016a4c
martiandreamer <martiandreamer@mail.com>
2023-11-10 00:35:07 +0700

    publish

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,3 @@
-import { Distributor } from "./distributor/distributor";
-import { concatWith, map } from "./operators";
-import { Subject } from "./subject";
-
-const sub: Subject<number> = new Subject();
-const sub2: Subject<string> = new Subject();
-const dist: Distributor<number | string> = sub.asDistributor().pipe(
-  map((e) => Math.floor(e * 100)),
-  concatWith(sub2)
-);
-dist.subscribe({ next: console.log });
-const int1 = setInterval(() => {
-  sub.publish(Math.random());
-}, 1000);
-
-setInterval(() => {
-  sub2.publish("hello");
-  console.log("hello is emitted");
-}, 4000);
-
-setTimeout(() => {
-  clearInterval(int1);
-  sub.complete();
-}, 5000);
+export { Distributor } from "./distributor/distributor";
+export { Subject } from "./subject";
+export { concatWith, filter, map, mergeWith } from "./operators";
