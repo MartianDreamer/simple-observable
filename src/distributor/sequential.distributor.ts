995547c252d481ba203ed35583fa4eaf857f21a8
nxsang <nxsang@axonactive.com>
2023-11-10 18:02:23 +0700

    add test

--- src/distributor/sequential.distributor.ts
+++ src/distributor/sequential.distributor.ts
@@ -2,24 +2,32 @@ import { Subscriber, Subscription } from "../interfaces";
 import { Distributor } from "./distributor";
 
 export class SequentialDistributor<T> extends Distributor<T> {
+  private currentSourceIndex: number = 0;
+  protected sourceSubscriber: Subscriber<T> = {
+    next: (event: T) => {
+      this.subscribers.forEach((subscriber: Subscriber<T>) => {
+        subscriber.next(event);
+      });
+    },
+    err: (err: Error) => {
+      this.subscribers.forEach((subscriber: Subscriber<T>) => {
+        if (subscriber.err) subscriber.err(err);
+      });
+    },
+    final: () => {
+      if (this.currentSourceIndex !== this.sources.length - 1) {
+        this.currentSourceIndex += 1;
+        this.sources[this.currentSourceIndex].subscribe(this.sourceSubscriber);
+      } else {
+        this.subscribers.forEach((subscriber: Subscriber<T>) => {
+          if (subscriber.final) subscriber.final();
+        });
+      }
+    },
+  };
   public subscribe(subscriber: Subscriber<T>): Subscription {
-    let i: number = 0;
-    if (this.sources[i] && !this.subscribedSources) {
-      this.subscribedSources = true;
-      const sourceSubscriber: Subscriber<T> = {
-        ...this.sourceSubscriber,
-        final: () => {
-          if (i !== this.sources.length - 1) {
-            i += 1;
-            this.sources[i].subscribe(sourceSubscriber);
-          } else {
-            this.subscribers.forEach((subscriber: Subscriber<T>) => {
-              if (subscriber.final) subscriber.final();
-            });
-          }
-        },
-      };
-      this.sources[i].subscribe(sourceSubscriber);
+    if (this.sources[0] && !this.subscribedSources) {
+      this.sources[0].subscribe(this.sourceSubscriber);
     }
     this.subscribers = [...this.subscribers, subscriber];
     return {
---
+++ src/distributor/sequentian.distributor.spec.ts
@@ -0,0 +1,30 @@
+import { concatWith } from "../operators";
+import { Subject } from "../subject";
+
+describe("sequential distributor", () => {
+  afterAll((done) => {
+    done();
+  });
+  test("concat 2 subject", () => {
+    const sub1 = new Subject<string>();
+    const sub2 = new Subject<string>();
+    const dist1 = sub1.asDistributor();
+    const dist2 = sub2.asDistributor();
+    const value: string[] = [];
+    const dist3 = dist1.pipe(concatWith(dist2));
+    dist3.subscribe({
+      next(data: string) {
+        value.push(data);
+      },
+    });
+    sub1.publish("s");
+    sub1.publish("s");
+    sub2.publish("t");
+    sub1.complete();
+    sub2.publish("t");
+    expect(value[0]).toBe("s");
+    expect(value[1]).toBe("s");
+    expect(value[2]).toBe("t");
+    expect(value.length).toBe(3);
+  });
+});
