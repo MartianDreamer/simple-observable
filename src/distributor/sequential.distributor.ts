982d252b0e8833fcdc8e3162c84a95b1c4f18f31
nxsang <nxsang@axonactive.com>
2023-11-13 16:18:10 +0700

    add some explanation

--- src/distributor/sequential.distributor.ts
+++ src/distributor/sequential.distributor.ts
@@ -37,6 +37,8 @@ export class SequentialDistributor<T> extends MultiSourceDistributor<T> {
             subscribed: true,
             complete: false,
           };
+          // check if all sources in sources subscription array completed then push new source into the array
+          // if all sources completed, subscribe the new source
           const allSourceAreCompleted: boolean = this.areAllSourcesCompleted();
           this.sourceSubscriptions.push(sourceSubscription);
           if (allSourceAreCompleted) {
@@ -63,11 +65,14 @@ export class SequentialDistributor<T> extends MultiSourceDistributor<T> {
       ...this.sourceSubscriber,
       complete: () => {
         sourceSubscription.complete = true;
+        // check if all the sources in sources array completed, if yes and source of sources completed as well, run all subscriber complete methods.
         const allSourceAreCompleted = this.areAllSourcesCompleted();
         if (allSourceAreCompleted && this.sourceOfSources.complete) {
           this.subscribers.forEach((subscriber: Subscriber<T>) => {
             if (subscriber.complete) subscriber.complete();
           });
+          // else if some sources has not completed, subscribe the next available source, if there is no available source, do nothing
+          // the next time source of sources emits a new source, all the sources have completed then the new source would be subscribed
         } else if (!allSourceAreCompleted) {
           const nextSourceIndex: number =
             this.sourceSubscriptions.indexOf(sourceSubscription) + 1;
